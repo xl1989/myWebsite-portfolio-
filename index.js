2563dfac831eeebaa7e53e20d9db7c1d185ed3e6
Xingming Li <xl1989@nyu.edu>
2018-07-03 16:47:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var cardFace=document.getElementById("cardFace");
 var cardBack=document.getElementById("cardBack");
 var toggle=document.getElementById("toggle");
 var lists=document.getElementById("lists");
+var conTitle=document.getElementById("conTitle");
+var conInfo=document.getElementById("conInfo");
+var proOne=document.getElementById("proOne");
+var ProThree=document.getElementById("ProThree");
+var proFive=document.getElementById("proFive");
 
 window.onscroll= function scrollAni(){
     if(document.body.scrollTop > 50){
@@ -15,6 +20,16 @@ window.onscroll= function scrollAni(){
            navBar.className= ""; 
         }
     
+    if(document.body.scrollTop > 350){
+        proOne.className="projects";
+        ProThree.className="projects";
+        proFive.className="projects";
+    }else{
+        proOne.className +=" ProUpHide";
+      ProThree.className +=" ProUpHide";
+        proFive.className +=" ProBottomHide";
+    }
+    
      if(document.body.scrollTop>1650){
          cardFace.style.transform="rotateY(-180deg)";
          cardBack.style.transform="rotateY(0deg)";
@@ -22,6 +37,15 @@ window.onscroll= function scrollAni(){
           cardFace.style.transform=null;
           cardBack.style.transform=null;
      }
+    
+     if(document.body.scrollTop>2000){
+         conTitle.className="contactShow";
+         conInfo.className="contactShow";
+     }else{
+         conTitle.className="conTitleHide";
+          conInfo.className="conInfoHide";
+     }
+    
 }
 
     
